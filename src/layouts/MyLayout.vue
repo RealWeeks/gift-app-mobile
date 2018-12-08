870e7bad1d2fa34cac2023052d218c7e02088825
RealWeeks <weeksjasons@gmail.com>
2018-12-08 13:46:20 -0500

    remove drawer, break header spacing

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-layout-header>
+    <!-- <q-layout-header>
       <q-toolbar
         color="red"
         :glossy="$q.theme === 'mat'"
@@ -18,12 +18,12 @@
 
         <q-toolbar-title>
           Gift App
-          <!-- <div slot="subtitle">Running on Quasar v{{ $q.version }}</div> -->
+          <div slot="subtitle">Running on Quasar v{{ $q.version }}</div>
         </q-toolbar-title>
       </q-toolbar>
-    </q-layout-header>
+    </q-layout-header> -->
 
-    <q-layout-drawer
+    <!-- <q-layout-drawer
       v-model="leftDrawerOpen"
       :content-class="$q.theme === 'mat' ? 'bg-grey-2' : null"
     >
@@ -54,7 +54,7 @@
           <q-item-main label="Twitter" sublabel="@quasarframework" />
         </q-item>
       </q-list>
-    </q-layout-drawer>
+    </q-layout-drawer> -->
 
     <q-page-container>
       <router-view />
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,10 @@
 <template>
   <q-page class="flex flex-start justify-center">
     <div class="column welcome-wrapper flex justify-around">
-      <div class="text-wrapper items-center welcome-banner">
-        <p class="items-center content-center text-weight-bolder header-text">Welcome to Gift App</p>
+      <div class="text-wrapper welcome-banner justify-center content-center items-center">
+        <p class="text-weight-bolder header-text self-cent">Welcome to</p>
+        <br/>
+        <p class="text-weight-bolder header-text self-center">Gift App</p>
       </div>
       <div class="text-wrapper items-center justify-between column text-weight-medium">
         <p>Create or Join a party</p>
@@ -23,6 +25,7 @@
 }
 .welcome-banner{
   margin-bottom: 25%;
+  text-align: center;
 }
 .header-text{
   font-size: 2.5em;
